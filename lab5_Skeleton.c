d1728606804878b06ffcf3ff5687578d27f01cf4
notkaramel <hoangtuan11102@gmail.com>
2022-07-12 01:24:55 -0400

    Add book->numNodes, fix deleteBook

--- lab5_Skeleton.c
+++ lab5_Skeleton.c
@@ -1,5 +1,5 @@
 //
-// APS105 Lab 5 
+// APS105 Lab 5
 //
 // Program for maintaining a personal phone book.
 //
@@ -23,17 +23,20 @@
 //
 //   ADD STATEMENT(S) HERE
 
-typedef struct node{
-    char* lname;
-    char* fname;
-    char* address;
-    char* phone;
-    struct node* next;
-}Node;
+typedef struct node
+{
+    char *lname;
+    char *fname;
+    char *address;
+    char *phone;
+    struct node *next;
+} Node;
 
-typedef struct llist{
-    Node* head;
-}LinkedList;
+typedef struct llist
+{
+    Node *head;
+    int numNode;
+} LinkedList;
 
 //**********************************************************************
 // Linked List Function Declarations
@@ -43,29 +46,29 @@ typedef struct llist{
 //
 //   ADD STATEMENT(S) HERE
 
-LinkedList* initlist();
-Node* newNode();
-void insertNode(LinkedList* book, Node* newnode);
-void deleteNode (char* lname,LinkedList* book);
-void checkPhoneNum(char* phonenum, LinkedList* book);
-void checkFamilyName(char* familyname,LinkedList* book);
+LinkedList *initlist();
+Node *newNode();
+void insertNode(LinkedList *book, Node *newnode);
+void deleteNode(char *lname, LinkedList *book);
+void checkPhoneNum(char *phonenum, LinkedList *book);
+void checkFamilyName(char *familyname, LinkedList *book);
 void printNode();
-void printList(LinkedList*book);
-void deleteBook(LinkedList* book, Node*temp);
+void printList(LinkedList *book);
+void deleteBook(LinkedList *book, Node *temp);
 
 //**********************************************************************
 // Support Function Declarations
 //
 
-void safegets (char s[], int arraySize);        // gets without buffer overflow
-void familyNameDuplicate (char familyName[]);   // marker/tester friendly
-void familyNameFound (char familyName[]);       //   functions to print
-void familyNameNotFound (char familyName[]);    //     messages to user
-void familyNameDeleted (char familyName[]);
-void phoneNumberFound (char phoneNumber[]);
-void phoneNumberNotFound (char phoneNumber[]);
-void printPhoneBookEmpty (void);
-void printPhoneBookTitle (void);
+void safegets(char s[], int arraySize);      // gets without buffer overflow
+void familyNameDuplicate(char familyName[]); // marker/tester friendly
+void familyNameFound(char familyName[]);     //   functions to print
+void familyNameNotFound(char familyName[]);  //     messages to user
+void familyNameDeleted(char familyName[]);
+void phoneNumberFound(char phoneNumber[]);
+void phoneNumberNotFound(char phoneNumber[]);
+void printPhoneBookEmpty(void);
+void printPhoneBookTitle(void);
 
 //**********************************************************************
 // Program-wide Constants
@@ -79,39 +82,35 @@ const char NEWLINE = '\n';
 // Main Program
 //
 
-int main (void)
+int main(void)
 {
-    const char bannerString[]
-        = "Personal Phone Book Maintenance Program.\n\n";
-    const char commandList[]
-        = "Commands are I (insert), D (delete), S (search by name),\n"
-          "  R (reverse search by phone #), P (print), Q (quit).\n";
+    const char bannerString[] = "Personal Phone Book Maintenance Program.\n\n";
+    const char commandList[] = "Commands are I (insert), D (delete), S (search by name),\n"
+                               "  R (reverse search by phone #), P (print), Q (quit).\n";
 
     // Declare linked list head.
     //   ADD STATEMENT(S) HERE TO DECLARE LINKED LIST HEAD.
-    
-    LinkedList* book = initlist();
+
+    LinkedList *book = initlist();
 
     // announce start of program
-    printf("%s",bannerString);
-    printf("%s",commandList);
+    printf("%s", bannerString);
+    printf("%s", commandList);
 
-    
-    char input[MAX_LENGTH+1];
-    while(1) // while(true)
+    char input[MAX_LENGTH + 1];
+    while (1) // while(true)
     {
         printf("\nCommand?: ");
-        safegets(input,MAX_LENGTH+1);
+        safegets(input, MAX_LENGTH + 1);
         // Response is first char entered by user.
         // Convert to uppercase to simplify later comparisons.
         char response = toupper(input[0]);
-        
+
         switch (response)
         {
         case 'I':
-            // Insert an phone book entry into the linked list.
+        { // Insert an phone book entry into the linked list.
             // Maintain the list in alphabetical order by family name.
-            //   ADD STATEMENT(S) HERE
 
             printf("  family name: ");
             char *lname = (char *)malloc(MAX_LENGTH + 1);
@@ -129,49 +128,61 @@ int main (void)
 
             insertNode(book, newnode);
             break;
+        }
         case 'D':
-            // Delete an phone book entry from the list
+        { // Delete an phone book entry from the list
             printf("\nEnter family name for entry to delete: ");
 
             char *input = (char *)malloc(MAX_LENGTH + 1);
             safegets(input, MAX_LENGTH);
             deleteNode(input, book);
             break;
+        }
         case 'S':
-            // Search for an phone book entry by family name.
+        { // Search for an phone book entry by family name.
             printf("\nEnter family name to search for: ");
 
             char familyname[MAX_LENGTH + 1];
             safegets(familyname, MAX_LENGTH);
             checkFamilyName(familyname, book);
             break;
-
+        }
         case 'R':
-            // Search for an phone book entry by phone number.
+        { // Search for an phone book entry by phone number.
             printf("\nEnter phone number to search for: ");
 
             char phonenum[MAX_LENGTH + 1];
             safegets(phonenum, MAX_LENGTH);
             checkPhoneNum(phonenum, book);
             break;
+        }
         case 'P':
-            // Print the phone book.
-            //   ADD STATEMENT(S) HERE
+        { // Print the phone book.
             printList(book);
             break;
+        }
+        case 'Q':
+        {
+            // Quit program/while loop
+            goto exit;
+        }
         default:
-            // do this if no command matched ...
+        { // do this if no command matched ...
             printf("\nInvalid command.\n%s\n", commandList);
             break;
         }
+        }
     }
+exit:
 
+    printf("Loop exited!\nBook as %d items!\n", book->numNode);
+    printList(book);
     // Delete the whole phone book linked list.
     deleteBook(book, book->head);
-    //free(book);
-    
+    // free(book);
+    // printList(book);
+
     // Print the linked list to confirm deletion.
-    printList(book);
     return 0;
 }
 
@@ -179,9 +190,9 @@ int main (void)
 // Support Function Definitions
 
 // Function to get a line of input without overflowing target char array.
-void safegets (char s[], int arraySize)
+void safegets(char s[], int arraySize)
 {
-    int i = 0, maxIndex = arraySize-1;
+    int i = 0, maxIndex = arraySize - 1;
     char c;
     while (i < maxIndex && (c = getchar()) != NEWLINE)
     {
@@ -193,56 +204,57 @@ void safegets (char s[], int arraySize)
 
 // Function to call when user is trying to insert a family name
 // that is already in the book.
-void familyNameDuplicate (char familyName[])
+void familyNameDuplicate(char familyName[])
 {
     printf("\nAn entry for <%s> is already in the phone book!\n"
-           "New entry not entered.\n",familyName);
+           "New entry not entered.\n",
+           familyName);
 }
 
 // Function to call when a family name was found in the phone book.
-void familyNameFound (char familyName[])
+void familyNameFound(char familyName[])
 {
     printf("\nThe family name <%s> was found in the phone book.\n",
-             familyName);
+           familyName);
 }
 
 // Function to call when a family name was not found in the phone book.
-void familyNameNotFound (char familyName[])
+void familyNameNotFound(char familyName[])
 {
     printf("\nThe family name <%s> is not in the phone book.\n",
-             familyName);
+           familyName);
 }
 
 // Function to call when a family name that is to be deleted
 // was found in the phone book.
-void familyNameDeleted (char familyName[])
+void familyNameDeleted(char familyName[])
 {
     printf("\nDeleting entry for family name <%s> from the phone book.\n",
-             familyName);
+           familyName);
 }
 
 // Function to call when a phone number was found in the phone book.
-void phoneNumberFound (char phoneNumber[])
+void phoneNumberFound(char phoneNumber[])
 {
     printf("\nThe phone number <%s> was found in the phone book.\n",
-             phoneNumber);
+           phoneNumber);
 }
 
 // Function to call when a phone number was not found in the phone book.
-void phoneNumberNotFound (char phoneNumber[])
+void phoneNumberNotFound(char phoneNumber[])
 {
     printf("\nThe phone number <%s> is not in the phone book.\n",
-             phoneNumber);
+           phoneNumber);
 }
 
 // Function to call when printing an empty phone book.
-void printPhoneBookEmpty (void)
+void printPhoneBookEmpty(void)
 {
     printf("\nThe phone book is empty.\n");
 }
 
 // Function to call to print title when whole phone book being printed.
-void printPhoneBookTitle (void)
+void printPhoneBookTitle(void)
 {
     printf("\nMy Personal Phone Book: \n");
 }
@@ -251,14 +263,17 @@ void printPhoneBookTitle (void)
 // Add your functions below this line.
 //   ADD STATEMENT(S) HERE
 
-LinkedList* initlist(){
-    LinkedList* list = (LinkedList*)malloc(sizeof(LinkedList));
-    list -> head = NULL;
+LinkedList *initlist()
+{
+    LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
+    list->head = NULL;
+    list->numNode = 0;
     return list;
 }
 
-Node* newNode(char* fname, char* lname, char* address, char* phonenum){
-    Node* node = (Node*)malloc(sizeof(Node));
+Node *newNode(char *fname, char *lname, char *address, char *phonenum)
+{
+    Node *node = (Node *)malloc(sizeof(Node));
     node->fname = fname;
     node->lname = lname;
     node->address = address;
@@ -267,69 +282,85 @@ Node* newNode(char* fname, char* lname, char* address, char* phonenum){
     return node;
 }
 
-void insertNode(LinkedList* book, Node* newnode){
-    
-    if (book->head == NULL){
+void insertNode(LinkedList *book, Node *newnode)
+{
+
+    if (book->numNode == 0)
+    {
         book->head = newnode;
     }
 
-    else if (strcmp(newnode->lname,book->head->lname) < 0){
+    else if (strcmp(newnode->lname, book->head->lname) < 0)
+    {
         newnode->next = book->head;
         book->head = newnode;
     }
-    
-    else{
-        Node* on = book->head;
-        
-        while ((strcmp(on->lname,newnode->lname) != 0) && on->next != NULL && strcmp(newnode->lname,on->next->lname) >= 0){
+
+    else
+    {
+        Node *on = book->head;
+
+        while ((strcmp(on->lname, newnode->lname) != 0) && on->next != NULL && strcmp(newnode->lname, on->next->lname) >= 0)
+        {
             on = on->next;
         }
-        if (strcmp(on->lname,newnode->lname) == 0){
+        if (strcmp(on->lname, newnode->lname) == 0)
+        {
             familyNameDuplicate(on->lname);
             return;
         }
         newnode->next = on->next;
         on->next = newnode;
     }
+    book->numNode++;
 }
 
-void deleteNode (char* lname,LinkedList* book){
-    if (book->head == NULL){
+void deleteNode(char *lname, LinkedList *book)
+{
+    if (book->head == NULL)
+    {
         familyNameNotFound(lname);
         return;
     }
 
-    Node* on = book->head;
+    Node *on = book->head;
 
-    while ((strcmp(on->lname,lname) != 0) && (on->next != NULL) && (strcmp(on->next->lname,lname) != 0)){
+    while ((strcmp(on->lname, lname) != 0) && (on->next != NULL) && (strcmp(on->next->lname, lname) != 0))
+    {
         printf("Entered the while loop\n");
         on = on->next;
     }
 
-    if (strcmp(on->lname,lname) == 0){
+    if (strcmp(on->lname, lname) == 0)
+    {
         book->head = on->next;
         free(on);
         printf("Deleted first node");
     }
-    else if (strcmp(on->next->lname,lname) == 0){
-        Node* temp = on->next;
+    else if (strcmp(on->next->lname, lname) == 0)
+    {
+        Node *temp = on->next;
         on->next = on->next->next;
         free(temp);
         familyNameDeleted(lname);
     }
-    
-    ///Problem here. The code doesn't even reach this place!!!!!
-    else if (on->next == NULL){
+
+    /// Problem here. The code doesn't even reach this place!!!!!
+    else if (on->next == NULL)
+    {
         printf("the code got here");
         familyNameNotFound(lname);
     }
-    return;
+    book->numNode--;
 }
 
-void checkFamilyName(char* familyname,LinkedList* book){
-    Node* on = book->head;
-    while (on != NULL){
-        if (strcmp(on->lname,familyname) == 0){
+void checkFamilyName(char *familyname, LinkedList *book)
+{
+    Node *on = book->head;
+    while (on != NULL)
+    {
+        if (strcmp(on->lname, familyname) == 0)
+        {
             familyNameFound(familyname);
             printNode(on);
             return;
@@ -339,26 +370,37 @@ void checkFamilyName(char* familyname,LinkedList* book){
     familyNameNotFound(familyname);
 }
 
-void checkPhoneNum(char* phonenum,LinkedList* book){
-    Node* on = book->head;
-    while (on != NULL){
-        if (strcmp(on->phone,phonenum) == 0){
+void checkPhoneNum(char *phonenum, LinkedList *book)
+{
+    Node *on = book->head;
+    while (on != NULL)
+    {
+        if (strcmp(on->phone, phonenum) == 0)
+        {
             phoneNumberFound(phonenum);
             printNode(on);
             return;
-        } 
+        }
         on = on->next;
     }
     phoneNumberNotFound(phonenum);
 }
 
-void printNode(Node* on){
-    printf("\n%s\n%s\n%s\n%s\n",on->lname, on->fname, on->address, on->phone);
+void printNode(Node *on)
+{
+    printf("\n%s\n%s\n%s\n%s\n", on->lname, on->fname, on->address, on->phone);
 }
 
-void printList(LinkedList* book){
-    Node* on = book->head;
-    while (on != NULL){
+void printList(LinkedList *book)
+{
+    if (book->numNode <= 0)
+    {
+        printPhoneBookEmpty();
+        return;
+    }
+    Node *on = book->head;
+    while (on != NULL)
+    {
         printNode(on);
         on = on->next;
     }
@@ -379,14 +421,14 @@ void printList(LinkedList* book){
 //     free(book);
 // }
 
-void deleteBook(LinkedList* book, Node* temp){
-    if(temp->next!=NULL)
-        deleteBook(book, temp->next);
+void deleteBook(LinkedList *book, Node *current)
+{
+    if (current->next != NULL)
+        deleteBook(book, current->next);
 
-    free(temp->fname);
-    free(temp->lname);
-    free(temp->phone);
-    free(temp->address);
-    free(temp);
-    
+    free(current->fname);
+    free(current->lname);
+    free(current->phone);
+    free(current->address);
+    free(current);
 }
